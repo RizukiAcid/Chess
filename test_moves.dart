962218cb05253154d0fb05092b984ab3c30b5428
RizukiAcid <muhammad.majiid@binus.ac.id>
2026-07-18 09:36:42 +0700

    chore: downgrade dependencies and Dart SDK version, and update test logic in test_moves.dart

--- test_moves.dart
+++ test_moves.dart
@@ -1,13 +1,8 @@
 import 'package:chess/chess.dart'; 
 void main() { 
   var game = Chess(); 
-  game.move({'from': 'e2', 'to': 'e4'});
-  game.move({'from': 'e7', 'to': 'e5'});
-  game.move({'from': 'g1', 'to': 'f3'});
-  game.move({'from': 'b8', 'to': 'c6'});
-  
-  for (var sq in ['e4', 'e5', 'f3', 'c6']) {
-    var p = game.get(sq);
-    print('Square: $sq, type: ${p?.type}, color: ${p?.color}');
-  }
+  var p = game.get('e2')!;
+  print('toString: "${p.type.toString()}"');
+  print('runtimeType: ${p.type.runtimeType}');
+  print('PieceType.PAWN: ${PieceType.PAWN}');
 }
